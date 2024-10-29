1c825e1f5585c4b55ce6b0a613b286c1dcdb75ad
RyuuShiro30 <annoraegaprasetyo>
2024-10-29 13:35:08 +0700

    array rata nilai modif

--- ArrayRataNilai08.java
+++ ArrayRataNilai08.java
@@ -4,9 +4,13 @@ public class ArrayRataNilai08 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
+        int jumlahsiswa = 0;
+        System.out.print("Masukkan jumlah siswa: ");
+        jumlahsiswa = input.nextInt();
+        int[] nilaiMhs = new int[jumlahsiswa];
         double total = 0;
         double ratarata;
+        int siswalulus= 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Nilai mahasiswa ke-" + (i+1) + " : ");
@@ -15,10 +19,14 @@ public class ArrayRataNilai08 {
         }
         for (int i = 0; i <nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                siswalulus += 1;
+            }
 
         }
         ratarata = total / nilaiMhs.length;
-        System.out.print("Rata-rata = " + ratarata);
+        System.out.println("Rata-rata = " + ratarata);
+        System.out.println("Mahasiswa nilai > 70 sebanyak: " + siswalulus);
     }
     
 }
