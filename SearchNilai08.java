20cd71b5bf26b577158c5ad21ba8e2fb98257c69
RyuuShiro30 <annoraegaprasetyo>
2024-10-29 14:09:08 +0700

    search nilai array modif

--- SearchNilai08.java
+++ SearchNilai08.java
@@ -4,21 +4,30 @@ public class SearchNilai08 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = input.nextInt();
+        int[] arrNilai = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = input.nextInt();
+        }
 
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = input.nextInt();
+        
+        int hasil = -1;
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+            if (arrNilai[i] == key) {
                 hasil = i;
                 break;
-                
             }
-            
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam data.");
+        }
     }
-    
 }
